4df011c1b820205ee01ab39e07db703f004aecdb
Seiyuu2 <kurtlylep@gmail.com>
2024-11-21 17:00:35 +0800

    The first trial run

---
+++ finalsproject/src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AdminDashboard from './pages/AdminDashboard';
+import LibrarianDashboard from './pages/LibrarianDashboard';
+import BorrowerDashboard from './pages/BorrowerDashboard';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/librarian" element={<LibrarianDashboard />} />
+        <Route path="/borrower" element={<BorrowerDashboard />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
